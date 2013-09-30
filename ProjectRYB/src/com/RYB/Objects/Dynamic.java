ea990b7edf79999bb8c101a654b80b604f48da29
Shane <Shane@67.194.115.162>
2013-09-30 18:19:47 -0400

    Commented collisions

--- ProjectRYB/src/com/RYB/Objects/Dynamic.java
+++ ProjectRYB/src/com/RYB/Objects/Dynamic.java
@@ -34,39 +34,41 @@ public abstract class Dynamic extends Entity {
     
     public void updateKinematics(){
         velocity.x = Math.signum(velocity.x)*(Math.abs(velocity.x)-FRICTION.x); //Apply friction
-        if(Math.abs(velocity.x)<FRICTION.x) velocity.x = 0;
+        if(Math.abs(velocity.x)<FRICTION.x) velocity.x = 0; //Round friction
         velocity.x += acceleration.x;
         velocity.y += acceleration.y;
         
-        prevx = x;
+        prevx = x; //Store previous x and y coordinates
         prevy = y;
         
         
+        //Update y coordinate and check vertical collisions
         y += velocity.y;
         for(int i = 0; i < world.getEntities().size(); i++){
             Entity e = (Entity) world.getEntities().get(i);
-            if(e instanceof Static) {
+            if(e instanceof Static) { //Loops through all static entities in the world
                 Static s = (Static)e;
-                if(this.isOverlap(s) && s.getSolid()) {
-                    y = this.overlapY(s);
-                    prevy = y-velocity.y;
+                if(this.isOverlap(s) && s.getSolid()) { //Check if the block is solid and overlapping
+                    y = this.overlapY(s); //Snap to edge of colliding block
+                    prevy = y-velocity.y; //For player class to recognize jumping should be true
                     velocity.y = 0;
                 }
             }
         }
+        //Update x coordinate and check horizontal collisions
         x += velocity.x;
         for(int i = 0; i < world.getEntities().size(); i++){
             Entity e = (Entity) world.getEntities().get(i);
-            if(e instanceof Static) {
+            if(e instanceof Static) { //Loop through all statics in the world
                 Static s = (Static)e;
-                if(this.isOverlap(s) && s.getSolid()) {
-                    x = this.overlapX(s);
+                if(this.isOverlap(s) && s.getSolid()) { //Check for collision with a solid
+                    x = this.overlapX(s); //Snap to edge of colliding block
                     velocity.x = 0;
                 }
             }
         }
         
-        if(y+h/2>Display.height) {
+        if(y+h/2>Display.height) { //Possibly temporary bottom of screen detect
             y=Display.height-h/2;
             prevy = y-velocity.y;
             velocity.y = 0;
